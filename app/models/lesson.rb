2c91d24cdc98142ccbed5a087d65bffd600b5297
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-31 10:21:27 -0500

    add lesson model class

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,24 @@
 class Lesson < ApplicationRecord
+  belongs_to :skill
+  belongs_to :student, class_name: "User"
+  validates :date, :name, presence: true
+  monetize :price_cents
+
+  def teacher
+    self.skill.teacher
+  end
+
+  def complete!
+    self.completed = true
+    self.save
+  end
+
+  def completed?
+    self.completed
+  end
+
+  def cancel!
+    self.completed = false
+    self.save
+  end
 end
