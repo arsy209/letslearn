d13d7d26a2da39db71404fcde3f4c042c9943212
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-30 23:03:24 -0500

    add skills migration

--- db/migrate/20180131012138_create_skills.rb
+++ db/migrate/20180131012138_create_skills.rb
@@ -1,6 +1,16 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
+      t.string :name
+      t.integer :price
+      t.text :description
+      t.string :availability
+      t.string :category
+      t.string :skill_rating
+
+      t.timestamps null: false
     end
+    add_reference :skills, :teacher, references: :users, index: true
+    add_foreign_key :skills, :users, column: :teacher_id
   end
 end
