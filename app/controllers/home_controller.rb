25dda819e2d86c393e997f526f9e127d6e0be353
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-30 23:38:02 -0500

    add under construction page tempory

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
 
   def home
   end
+
+  def under_construction
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "home#home"
+  # root "home#home"
+  root "home#under_construction"
 
   resources :skills do
     post 'lesson_sms',to: 'notifications#notify_lesson_request'
