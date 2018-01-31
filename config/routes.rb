459ace64ac28563a6bfbe2f012cdd7516989b4a3
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-30 23:25:47 -0500

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#home"
+
+  resources :skills do
+    post 'lesson_sms',to: 'notifications#notify_lesson_request'
+    resources :reviews, only: :create
+  end
+  resources :lessons, only: [:index, :create, :show, :update] do
+    resources :reviews, only: :new
+  end
+  resources :messages, only: [:index, :create, :show, :new]
+  resources :profiles, only: :show
+
 end
