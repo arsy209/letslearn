f2ad49183963e56cfc687b968fa9740142b4bc98
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-30 23:04:00 -0500

    add lessons migration

--- db/migrate/20180131012146_create_lessons.rb
+++ db/migrate/20180131012146_create_lessons.rb
@@ -1,6 +1,14 @@
 class CreateLessons < ActiveRecord::Migration[5.1]
   def change
     create_table :lessons do |t|
+      t.references :skill, foreign_key: true
+      t.string :name
+      t.string :description
+      t.boolean :completed
+      t.boolean :paid
+      t.string :price
+
+      t.timestamps null: false
     end
   end
 end
