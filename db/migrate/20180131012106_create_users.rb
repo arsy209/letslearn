16ca6a18400fc21676becb09f0a5d18d51eba688
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-31 21:17:51 -0500

    edit user schema

--- db/migrate/20180131012106_create_users.rb
+++ db/migrate/20180131012106_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :email, null: false
       t.string :password_digest, null: false
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
