fa459951c95954a6397a7656d31fec837ee3259e
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-31 21:16:11 -0500

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
-  
+
+  has_attached_file :avatar, default_url: "/images/:style/missing.png"
+validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   has_many :skills, foreign_key: :teacher_id
   has_many :lessons, foreign_key: :student_id
   has_many :reviews, foreign_key: :student_id
