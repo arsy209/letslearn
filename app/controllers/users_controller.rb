6972218fd36235f35efd79e1646058a788312a20
Arshdeep Singh <singh_arsy@yahoo.com>
2018-01-31 21:15:36 -0500

    fix users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    if current_user
+  redirect_to root_path
+else
+  @user = User.new
+end
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:success] = "Welcome #{@user.username}"
-      redirect_to user_path(@user)
-    end
+      if @user.save
+        redirect_to root_path, notice: 'Account Creation Successful!'
+      else
+        render 'new'
+      end
   end
 
   def edit
@@ -22,7 +26,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:success]= "Your account was updated successfully"
-      redirect_to articles_path
+      redirect_to root_path
     else
       render 'edit'
     end
@@ -34,7 +38,7 @@ class UsersController < ApplicationController
 
   private
 
-  def users_params
-    params.require(:user).permit(:photo,:email,:first_name,:last_name,:bio, :password)
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:avatar,:email,:bio, :password,:role)
   end
 end
