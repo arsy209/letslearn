af42f5cf66e4465d134c0a980fa51fa8d9d012d6
Arshdeep Singh <singh_arsy@yahoo.com>
2018-02-01 19:12:13 -0500

    fix helper method errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,22 @@
 module ApplicationHelper
   def avatar_url
-    return "#{current_user.facebook_picture_url}&height=50&width=50" if current_user.facebook_picture_url
+    return "#{current_user.avatar}&height=50&width=50" if current_user.avatar
     current_user.photo_url(:thumb) || "http://placehold.it/30x30"
   end
 
   def photo_url(user)
-    return "#{user.facebook_picture_url}&height=200&width=200" if user.facebook_picture_url
+    return "#{user.avatar}&height=200&width=200" if user.avatar
     user.photo_url(:profile) || "http://placehold.it/30x30"
   end
 
   def teacher_photo(skill)
-    return "#{skill.teacher.facebook_picture_url}&height=50&width=50" if skill.teacher.facebook_picture_url
+    return "#{skill.teacher.avatar}&height=50&width=50" if skill.teacher.avatar
     skill.teacher.photo.url(:thumb) ||
     "http://placehold.it/30x30"
   end
 
   def small_user_photo(user)
-    return "#{user.facebook_picture_url}&height=50&width=50" if user.facebook_picture_url
+    return "#{user.avatar}&height=50&width=50" if user.avatar
     user.photo.url(:thumb) || "http://placehold.it/30x30"
   end
 
