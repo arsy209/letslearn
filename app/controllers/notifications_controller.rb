685e46462911b135642e3dcb342d486fcc7a4e5b
Arshdeep Singh <singh_arsy@yahoo.com>
2018-02-06 19:56:59 -0500

    minor change

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,6 +4,6 @@ class NotificationsController < ApplicationController
   def notify_lesson_request
     teacher = Skill.find(params[:skill_id]).teacher
     client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-    message = client.messages.create from: ENV['TWILIO_NUMBER'], to: teacher.phone, body: 'Hello from Lealo!\nYou received a new lesson request!'
+    message = client.messages.create from: ENV['TWILIO_NUMBER'], to: teacher.phone, body: 'Hello! \nYou received a new lesson request!'
   end
 end
