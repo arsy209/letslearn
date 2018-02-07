9c75e9f683c51e09d19a251fb04c7f381e6c952f
Arshdeep Singh <singh_arsy@yahoo.com>
2018-02-07 17:14:50 -0500

    render new form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,9 @@ class SessionsController < ApplicationController
       flash[:success] = "You have successfully logged in"
       redirect_to profile_path(current_user)
     else
+        render "new"
       flash.now[:danger] = "There was something wrong with your login information"
+
     end
   end
 
